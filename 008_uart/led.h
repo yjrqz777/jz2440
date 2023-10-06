eb19d04228b41e8fcab09daef606ba6eafcd46c4
root <root@book-virtual-machine>
2023-10-06 18:06:46 +0800

    led_plus

---
+++ 008_uart/led.h
@@ -0,0 +1,15 @@
+
+
+#ifndef _LED_H
+#define _LED_H
+
+
+void delay(int d);
+int led_test();
+
+void led_init();
+
+void open_led(int led);
+
+
+#endif
--- 008_uart/main.c
+++ 008_uart/main.c
@@ -1,13 +1,13 @@
 
 #include "s3c2440_soc.h"
 #include "uart.h"
-
+#include "led.h"
 
 int main()
 {
     unsigned char c;
     uart0_init();
-
+    led_init();
     puts("Hello, world!\n\r");
 
 
@@ -19,7 +19,14 @@ int main()
         if(c=='\r')
             putchar('\n');
         if(c=='\n')
-        putchar('\r');
+            putchar('\r');
+
+        if(c=='1')
+            open_led(1);
+        if(c=='2')
+            open_led(2);
+        if(c=='3')
+            open_led(3);
         putchar(c);
 
     }
