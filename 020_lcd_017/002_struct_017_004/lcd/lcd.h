b6f0354703b57a06704662ed6decf402d08dabb6
root <root@book-virtual-machine>
2023-11-05 21:35:00 +0800

    11-5

---
+++ 020_lcd_017/002_struct_017_004/lcd/lcd.h
@@ -0,0 +1,56 @@
+
+#ifndef _LCD_H_
+#define _LCD_H_
+
+enum {
+    NORMAL = 0,
+    INVERT = 1,
+};
+
+
+
+typedef struct pins_polarity {
+    int vclk;           // noraml: 下降岩
+    int rgb;            // normal: 高电平有效   
+    int hsync;          // normal: 高电平有效
+    int vsync;          // normal: 高电平有效
+}pins_polarity, *p_pins_polarity;
+
+typedef struct time_sequence {
+    //垂直方向
+    int tvp;            // vysnc 脉冲宽度
+    int tvb;            // vysnc 后的黑框，上
+    int tvf;            // vysnc 前的黑框，下
+
+
+    //水平方向
+    int thp;            // hsync 脉冲宽度
+    int thb;            // hsync 后的黑框，左
+    int thf;            // hsync 前的黑框，右
+
+
+
+}time_sequence, *p_time_sequence;
+
+
+
+typedef struct lcd_params{
+    //引脚
+    pins_polarity pins_pol;
+    //时序
+    time_sequence time_seq;
+    //分辨率 bpp
+    int xres;
+    int yres;
+    int bpp;
+    //framebuffer 地址
+    unsigned int fb_base;
+
+}lcd_params, *p_lcd_params;
+
+
+
+
+
+
+#endif
---
+++ 020_lcd_017/002_struct_017_004/lcd/s3c2440_lcd_controller.c
@@ -0,0 +1,8 @@
+
+struct lcd_controller s3c2440_lcd_controler={
+    .init=lcd_controller_init,
+    .enable=lcd_controller_enable,
+    .disable=lcd_controller_disable,
+};
+
+
